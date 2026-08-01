3d14da20d7489304025d7bb46b0e60ccf73f8e9a
Bala Murugan <bmg311204@gmail.com>
2026-08-01 18:50:41 +0530

    updated modifier.py

--- src/GoldPriceTracker/modifier.py
+++ src/GoldPriceTracker/modifier.py
@@ -2,39 +2,49 @@ import json
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-today = str(datetime.now(ZoneInfo("Asia/Kolkata")))
-
-data = ""
 
 def check_and_modify(todays_price):
+    today_str =  datetime.now(ZoneInfo("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")
+    status_code = 0
     try:
         with open("./price_tracker.json", "r") as file:
             data = json.load(file)
-            max_price = data["max_price"]
-            percent_change = ((max_price-todays_price)/max_price) * 100
-            if (todays_price > max_price):
-                max_price = todays_price
-                return -1
-            elif percent_change >= 5.0:
-                return 1
+        max_price = data["max_price"]
+        percent_change = ((max_price-todays_price)/max_price) * 100
+        if (todays_price > max_price):
+            max_price = todays_price
+            status_code = -1
+        elif percent_change >= 5.0:
+            return 1
+
+        data["max_price"] = max_price
+        data["history"].append({
+            "timestamp": today_str,
+            "price": todays_price,
+            "percent_change": round(percent_change, 2)
+        })
+
 
         with open("./price_tracker.json", "w") as file:
-            data["max_price"] = max_price
-            data["history"].append({today: todays_price, "change in percent": percent_change
-                                    })
             json.dump(data, file, indent=4)
+
+        return status_code
             
 
     except:
         with open("./price_tracker.json", "w") as file:
-            data = {}
-            history = []
-            history.append({today : todays_price})
             data = {
                 "max_price": todays_price,
-                "history": history
+                "history": [
+                    {
+                        "timestamp": today_str,
+                        "price": todays_price,
+                        "percent_change": 0.0
+                    }
+                ]
             }
-            json.dump(data, file, indent=4)
+            with open("./price_tracker.json","w") as file:
+                json.dump(data, file, indent=4)
             return 0
 
 
