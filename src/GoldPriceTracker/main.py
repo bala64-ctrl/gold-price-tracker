722abdfb7189a082c392fad534563e95bd68c0d5
Bala Murugan <bmg311204@gmail.com>
2026-08-01 17:26:08 +0530

    updated main.py

--- src/GoldPriceTracker/main.py
+++ src/GoldPriceTracker/main.py
@@ -1,14 +1,10 @@
-from fetcher import fetch
-from modifier import check_and_modify
-from notifier import notify
-
 def main():
+    from fetcher import fetch
+    from modifier import check_and_modify
+    from notifier import notify    
     todays_price = fetch()
     todays_status = check_and_modify(todays_price)
     notify(todays_status)
-    from fetcher import fetch
-    from modifier import check_and_modify
-    from notifier import notify
 
 
     todays_price = fetch()
